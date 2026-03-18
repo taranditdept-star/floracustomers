72b60ad3acfadc656bf931823244550087113635
pearmicaiah <pearmicaiah@gmail.com>
2026-03-18 13:02:24 +0200

    fix: Convert next.config.js to proper JavaScript syntax

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   reactCompiler: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
